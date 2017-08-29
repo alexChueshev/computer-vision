095dd542093ba93470c0c48e0e0e3863f8bee8d6
alexander <alexchueshev@gmail.com>
2017-08-29 17:08:35 +0200

    * changed type of grayscale image

--- lib/src/operations.cpp
+++ lib/src/operations.cpp
@@ -1,26 +1,23 @@
 #include "operations.h"
 
 void pi::opts::grayscale(cv::Mat &src) {
-    assert(src.depth() == CV_8U);
-    assert(src.channels() == 3);
+    assert(src.type() == CV_8UC3);
 
-    cv::Mat grayscale(src.rows, src.cols, CV_8UC1);
+    cv::Mat grayscale(src.rows, src.cols, CV_32FC1);
 
     auto srcIt = src.begin<cv::Vec3b>(),
             srcEnd = src.end<cv::Vec3b>();
 
-    for (auto gIt = grayscale.begin<uchar>(),
-                 gEnd = grayscale.end<uchar>(); gIt != gEnd; ++srcIt, ++gIt) {
-        *gIt = cv::saturate_cast<uchar>
-                (.299 * (*srcIt)[2] + .587 * (*srcIt)[1] + .114 * (*srcIt)[0]);
+    for (auto gIt = grayscale.begin<float>(),
+                 gEnd = grayscale.end<float>(); gIt != gEnd; ++srcIt, ++gIt) {
+        *gIt = (.299 * (*srcIt)[2] + .587 * (*srcIt)[1] + .114 * (*srcIt)[0]) / 255;
     }
 
     src = std::move(grayscale);
 }
 
 void pi::opts::normalize(cv::Mat &src) {
-    assert(src.depth() == CV_8U);
-    assert(src.channels() == 1);
+    assert(src.type() == CV_32FC1);
 
     auto min = .0, max = .0;
 
