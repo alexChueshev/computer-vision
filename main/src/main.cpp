dfe5ab88d9910d3c2ffef84f4b21525996d9793d
alexander <alexchueshev@gmail.com>
2017-09-21 06:55:51 +0200

    * upgraded project for qt

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -6,46 +6,15 @@
 #include <filters.h>
 
 using namespace pi;
-using namespace std;
-struct A {
-    A() {
-        cout << "def c" << endl;
-    }
-
-    A(const A &a) {
-        cout << "copy c" << endl;
-    }
-
-    A(A &&a) {
-        cout << "move c" << endl;
-    }
-
-    A& operator=(A &&a) {
-        cout << "move =" << endl;
-    }
-
-    A& operator=(const A &a) {
-        cout << "copy =" << endl;
-    }
-};
-
-int test(A a) {
-    A b = std::move(a);
-    int c = 2;
-}
 
 int main() {
-
-    A a;
-    test(a);
-
     auto oImage = utils::loadImage("/home/alexander/Lenna.png");
     auto pImage = oImage.clone();
 
     pImage.operations({pi::opts::grayscale,
                        pi::opts::normalize,
                        [](cv::Mat &src) {
-                           filters::Gaussian(1).apply(src, borders::BORDER_REFLECT);
+                           filters::Gaussian(2).apply(src, borders::BORDER_REFLECT);
                            filters::Sobel().apply(src, borders::BORDER_REFLECT);
                        }
                       });
