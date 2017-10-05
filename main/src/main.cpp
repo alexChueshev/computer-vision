b2f2655967c5ffd768344d21a1ba7bd74285b3e6
alexander <alexchueshev@gmail.com>
2017-10-05 22:02:39 +0700

    * added to main content of 1 lab

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -5,8 +5,35 @@
 #include <utils.h>
 
 using namespace pi;
+using namespace std::placeholders;
+
+void l1();
+
+void l2();
 
 int main() {
+    //l1();
+    l2();
+
+    return 0;
+}
+
+void l1() {
+    ImageProcessing imageProcessing("/home/alexander/Lenna.png", utils::load);
+
+    filters::Sobel sobel;
+    auto sobelFunction = std::bind(&filters::Sobel::apply, &sobel, _1, _2);
+
+    filters::Gaussian gaussian(2.5);
+    auto gaussianFunction = std::bind(&filters::Gaussian::apply, &gaussian, _1, _2);
+
+    imageProcessing.opts({opts::grayscale, opts::normalize})
+                   .filters({std::make_pair(borders::BORDER_REPLICATE, gaussianFunction),
+                             std::make_pair(borders::BORDER_REPLICATE, sobelFunction)})
+                    .render("result", utils::render);
+}
+
+void l2() {
     pyramids::GaussianPyramid pyramid;
     ImageProcessing imageProcessing("/home/alexander/Lenna.png", utils::load);
 
@@ -15,6 +42,4 @@ int main() {
         utils::save("/home/alexander/lr2/" + std::to_string(layer.sigmaEffective) + ".png",
                     layer.img);
     });
-
-    return 0;
 }
