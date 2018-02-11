4dea7ee65ed0411f3b0f2cae21627ebbeae2e92a
alexander <alexchueshev@gmail.com>
2018-02-11 21:00:55 +0700

    * additional logger endpoints

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -9,19 +9,23 @@
 using namespace pi;
 using namespace boost;
 
-constexpr auto EMPTY_IMG = "[Error] - can't load images";
-constexpr auto NO_MATCHES = "[Warning] - can't provide enough feature matches: ";
-constexpr auto OBJ_FOUND = "[Success] - object is found in: ";
+constexpr auto EMPTY_IMG = "[Error] - Can't load images";
+constexpr auto OBJ_NOT_FOUND = "[Warning] - Can't find object in: ";
+constexpr auto OBJ_FOUND = "[Success] - Object is found in: ";
 constexpr auto INFO = "[Info] - For more information, see the destination path";
+constexpr auto SEARCH = "[Info] - Searching into: ";
+constexpr auto COMPUTE = "[Info] - Computing descriptors of: ";
 
 std::vector<descriptors::SiDescriptor> descriptors(const pi::Img&);
 std::vector<cv::Point2f> rect(const pi::Img&, const transforms::Transform2d&, float);
 
 int main(int argc, char *argv[]) {
-    cv::CommandLineParser parser(argc, argv, "{obj||}{src||}{dst||}");
+    cv::CommandLineParser parser(argc, argv, "{obj|<none>|}{src|<none>|}{dst|<none>|}{thrs|.75|}{p|.7|}");
     const auto objPath = parser.get<std::string>("obj");
     const auto srcPath = parser.get<std::string>("src");
     const auto dstPath = parser.get<std::string>("dst");
+    const auto threshold = parser.get<float>("thrs");
+    const auto p = parser.get<float>("p");
 
     if(!parser.check()) {
         parser.printErrors();
@@ -41,6 +45,7 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
+    LOGGER(::COMPUTE + objPath);
     object = opts::normalize(opts::grayscale(object));
     auto objDescriptors = ::descriptors(object);
 
@@ -48,14 +53,17 @@ int main(int argc, char *argv[]) {
         const auto &img = image.second;
         const auto filename = filesystem::path{dstPath} /= filesystem::path{image.first}.stem();
 
-        auto matches = descriptors::match<detectors::SPoint>(objDescriptors, ::descriptors(img));
+        LOGGER(::COMPUTE + image.first);
+        auto matches = descriptors::match<detectors::SPoint>(objDescriptors, ::descriptors(img), p);
+
         if(matches.size() < 3) {
-            LOGGER(::NO_MATCHES + image.first);
+            LOGGER(::OBJ_NOT_FOUND + image.first);
             continue;
         }
 
+        LOGGER(::SEARCH + image.first);
         auto vHypothesis = transforms::verify(transforms::hough(img.dimensions(), object.dimensions(), matches)
-                                              , matches.size(), .7f);
+                                              , matches.size(), threshold);
         const auto &transform2d = vHypothesis.first;
         const auto probability = vHypothesis.second;
 
@@ -65,6 +73,8 @@ int main(int argc, char *argv[]) {
             utils::save(filename.string(), utils::addRectTo(img, ::rect(object, transform2d.first, 5.f)));
 
             LOGGER(::OBJ_FOUND + image.first);
+        } else {
+            LOGGER(::OBJ_NOT_FOUND + image.first);
         }
     }
 
@@ -75,9 +85,9 @@ int main(int argc, char *argv[]) {
 
 std::vector<descriptors::SiDescriptor> descriptors(const pi::Img& img) {
     auto gpyramid = pyramids::gpyramid(img, 3, 3, pyramids::logOctavesCount);
-    auto dog = pyramids::dog(gpyramid);
+    auto dog = pyramids::dog(gpyramid); auto blobs = detectors::blobs(dog);
 
-    return descriptors::siDescriptors(detectors::shiTomasi(dog, detectors::blobs(dog), 1e-5f)
+    return descriptors::siDescriptors(detectors::shiTomasi(dog, blobs, 8e-6f)
                                       , gpyramid, [](const auto& descriptor) {
         return descriptors::normalize(descriptors::trim(descriptors::normalize(descriptor)));
     });
